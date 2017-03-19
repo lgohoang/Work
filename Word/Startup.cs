9f864bd1d85f384419f360e3bd4f55e17cb28589
Hoàng Nguyễn <Hong Nguyn>
2017-03-19 21:06:23 +0700

    Hello

---
+++ Word/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Word.Startup))]
+namespace Word
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
